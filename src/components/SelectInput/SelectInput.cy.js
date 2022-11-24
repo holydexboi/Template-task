9cd287066ddd445544d44f981a8300628239d94e
Richard <holydexboi@yahoo.com>
2022-11-24 21:51:02 +0100

    add option

--- src/components/SelectInput/SelectInput.cy.js
+++ src/components/SelectInput/SelectInput.cy.js
@@ -1,8 +1,13 @@
 import SelectInput from "./SelectInput";
+const orderOptions = [
+    { label: "Default", value: "Default" },
+    { label: "Ascending", value: "Ascending" },
+    { label: "Descending", value: "Descending" },
+  ];
 
 describe('<SelectInput>', () => {
     it('mount', () => {
         // eslint-disable-next-line no-undef
-        cy.mount(<SelectInput/>)
+        cy.mount(<SelectInput options={orderOptions}/>)
     })
 })
